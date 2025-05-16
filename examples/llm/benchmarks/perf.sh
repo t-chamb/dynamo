fc90a593226a927f5c56b07baada531263fce05b
Ryan McCormick <rmccormick@nvidia.com>
2025-05-15 20:21:57 -0700

    [DO NOT MERGE] Add scripts used for benchmarking and plotting in experiments thus far

--- examples/llm/benchmarks/perf.sh
+++ examples/llm/benchmarks/perf.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,27 +14,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-model=neuralmagic/DeepSeek-R1-Distill-Llama-70B-FP8-dynamic
+# dynamo + trtllm
+model=nvidia/DeepSeek-R1-FP4
+tokenizer="${model}"
 
-# Input Sequence Length (isl) 3000 and Output Sequence Length (osl) 150 are
-# selected for chat use case. Note that for other use cases, the results and
-# tuning would vary.
-isl=3000
-osl=150
+host="localhost"
+
+# TODO: Why does genai-perf with isl=8192 actually send 8195 tokens?
+# RuntimeError: [TensorRT-LLM][ERROR] Assertion failed: The number of context tokens (8195) exceeds the limit value (8192)
+# FIXME: Use isl=8000 as a WAR to avoid exceeding 8192 max seq_len for prefill
+isl=8000
+osl=256
+
+# NOTE: putting ignore_eos inside nvext is currently required when benchmarking
+# dynamo -- the dynamo OpenAI frontend won't propagate ignore_eos outside of
+# of the nvext json field.
 
 # Concurrency levels to test
 for concurrency in 1 2 4 8 16 32 64 128 256; do
-
-  # NOTE: For Dynamo HTTP OpenAI frontend, use `nvext` for fields like
-  # `ignore_eos` since they are not in the official OpenAI spec.
   genai-perf profile \
     --model ${model} \
-    --tokenizer ${model} \
+    --tokenizer ${tokenizer} \
     --service-kind openai \
     --endpoint-type chat \
     --endpoint /v1/chat/completions \
     --streaming \
-    --url http://localhost:8000 \
+    --url ${host}:8000 \
     --synthetic-input-tokens-mean ${isl} \
     --synthetic-input-tokens-stddev 0 \
     --output-tokens-mean ${osl} \
@@ -55,3 +60,4 @@ for concurrency in 1 2 4 8 16 32 64 128 256; do
     -H 'Accept: text/event-stream'
 
 done
+
